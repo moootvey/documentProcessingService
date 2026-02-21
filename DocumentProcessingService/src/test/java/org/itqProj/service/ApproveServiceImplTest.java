d627cb3c4db618b030f9424a0f9f59304ccba0d9
moootvey <m.slabukhin@gmail.com>
2026-02-22 01:14:21 +0700

    Add unit tests for ApproveServiceImpl and ApproveTransactionServiceImpl

---
+++ DocumentProcessingService/src/test/java/org/itqProj/service/ApproveServiceImplTest.java
@@ -0,0 +1,110 @@
+package org.itqProj.service;
+
+import org.itqProj.dto.model.ApproveResponseModelDto;
+import org.itqProj.dto.response.ApproveResponseDto;
+import org.itqProj.enums.ApproveStatusEnum;
+import org.itqProj.service.interfaces.ApproveService;
+import org.itqProj.service.interfaces.ApproveTransactionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class ApproveServiceImplTest {
+
+    @Mock
+    private ApproveTransactionService approveTransactionService;
+
+    @InjectMocks
+    private ApproveServiceImpl approveService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void submitSingleDocument_HappyPath() {
+        when(approveTransactionService
+                .submitOneDocument(
+                        anyString(),
+                        anyString(),
+                        anyString()))
+                .thenReturn(new ApproveResponseModelDto(
+                        "24dba5928c9b4f59",
+                        ApproveStatusEnum.SUCCESSFULLY,
+                        "ok"));
+
+        ApproveResponseDto response = approveService.submitDocuments(
+                List.of("24dba5928c9b4f59"),
+                "John Doe",
+                "Details test mess");
+
+        assertEquals(1, response.getApproveRequestResult().size());
+        assertEquals(ApproveStatusEnum.SUCCESSFULLY, response.getApproveRequestResult().get(0).getApproveStatus());
+    }
+
+    @Test
+    void submitBatch_HappyPath() {
+        when(approveTransactionService
+                .submitOneDocument(
+                        anyString(),
+                        anyString(),
+                        anyString()))
+                .thenAnswer(
+                        invocation ->
+                                new ApproveResponseModelDto((String) invocation.getArgument(0), ApproveStatusEnum.SUCCESSFULLY, "ok"));
+
+        ApproveResponseDto response = approveService.submitDocuments(
+                List.of("24dba5928c9b4f59", "05cc11bfe69145fa", "c8c5cf480db84d70"),
+                "John Doe",
+                "Details test mess");
+
+        assertEquals(3, response.getApproveRequestResult().size());
+        assertEquals("24dba5928c9b4f59", response.getApproveRequestResult().get(0).getDocumentId());
+        assertEquals("05cc11bfe69145fa", response.getApproveRequestResult().get(1).getDocumentId());
+        assertEquals("c8c5cf480db84d70", response.getApproveRequestResult().get(2).getDocumentId());
+    }
+
+    @Test
+    void approveBatch_PartialResults() {
+        when(approveTransactionService
+                .approveOneDocument(
+                        "c8c5cf480db84d70",
+                        "John Doe",
+                        "Details test mess"))
+                .thenReturn(
+                        new ApproveResponseModelDto(
+                                "c8c5cf480db84d70",
+                                ApproveStatusEnum.SUCCESSFULLY,
+                                "ok"));
+
+        when(approveTransactionService
+                .approveOneDocument(
+                        "05cc11bfe69145fa",
+                        "John Doe",
+                        "Details test mess"))
+                .thenReturn(
+                        new ApproveResponseModelDto(
+                                "05cc11bfe69145fa",
+                                ApproveStatusEnum.CONFLICT,
+                                "conflict"));
+
+        ApproveResponseDto response = approveService.approveDocuments(
+                List.of("c8c5cf480db84d70", "05cc11bfe69145fa"),
+                "John Doe",
+                "Details test mess");
+
+        assertEquals(2, response.getApproveRequestResult().size());
+        assertEquals(ApproveStatusEnum.SUCCESSFULLY, response.getApproveRequestResult().get(0).getApproveStatus());
+        assertEquals(ApproveStatusEnum.CONFLICT, response.getApproveRequestResult().get(1).getApproveStatus());
+    }
+}
+
---
+++ DocumentProcessingService/src/test/java/org/itqProj/service/ApproveTransactionServiceImplTest.java
@@ -0,0 +1,160 @@
+package org.itqProj.service;
+
+import org.itqProj.dto.model.ApproveResponseModelDto;
+import org.itqProj.entity.ApproveRegisterEntity;
+import org.itqProj.entity.DocumentMetadataEntity;
+import org.itqProj.enums.ApproveStatusEnum;
+import org.itqProj.enums.DocumentStatusEnum;
+import org.itqProj.exception.DocumentNotFoundException;
+import org.itqProj.repository.ApproveRegisterRepository;
+import org.itqProj.repository.DocumentMetadataRepository;
+import org.itqProj.util.GeneratorUUIDUtil;
+import org.itqProj.util.MapperUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataAccessException;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import static java.time.ZoneOffset.UTC;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class ApproveTransactionServiceImplTest {
+
+    @Mock
+    private DocumentMetadataRepository documentMetadataRepository;
+
+    @Mock
+    private ApproveRegisterRepository approveRegisterRepository;
+
+    @InjectMocks
+    private ApproveTransactionServiceImpl approveTransactionService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void approveOneDocument_HappyPath() {
+        when(documentMetadataRepository
+                .findByIdForUpdate("406b41bcc7ba4fce"))
+                .thenReturn(Optional.of(new DocumentMetadataEntity(
+                        "406b41bcc7ba4fce",
+                        "doc_406b41bcc7ba4fce_2026-02-21T09:38:37.459963Z",
+                        "John Doe",
+                        "Test Document",
+                        DocumentStatusEnum.SUBMITTED,
+                        OffsetDateTime.now(UTC),
+                        OffsetDateTime.now(UTC),
+                        null)));
+
+        doNothing()
+                .when(documentMetadataRepository)
+                .updateStatusByDocumentIds(
+                        eq("406b41bcc7ba4fce"),
+                        eq(DocumentStatusEnum.APPROVED),
+                        any(OffsetDateTime.class));
+
+        when(approveRegisterRepository
+                .save(any(ApproveRegisterEntity.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        ApproveResponseModelDto result = approveTransactionService
+                .approveOneDocument(
+                        "406b41bcc7ba4fce",
+                        "John Doe",
+                        "Test Document");
+
+        assertNotNull(result);
+        assertEquals(ApproveStatusEnum.SUCCESSFULLY, result.getApproveStatus());
+
+        verify(documentMetadataRepository, times(1))
+                .updateStatusByDocumentIds(
+                        eq("406b41bcc7ba4fce"),
+                        eq(DocumentStatusEnum.APPROVED),
+                        any(OffsetDateTime.class));
+        verify(approveRegisterRepository, times(1))
+                .save(any(ApproveRegisterEntity.class));
+    }
+
+    @Test
+    void approveOneDocument_RollbackOnRegisterSaveError() {
+        when(documentMetadataRepository
+                .findByIdForUpdate("406b41bcc7ba4fce"))
+                .thenReturn(Optional.of(new DocumentMetadataEntity(
+                        "406b41bcc7ba4fce",
+                        "doc_406b41bcc7ba4fce_2026-02-21T09:38:37.459963Z",
+                        "John Doe",
+                        "Test Document",
+                        DocumentStatusEnum.SUBMITTED,
+                        OffsetDateTime.now(UTC),
+                        OffsetDateTime.now(UTC),
+                        null)));
+
+        doNothing()
+                .when(documentMetadataRepository)
+                .updateStatusByDocumentIds(
+                        eq("406b41bcc7ba4fce"),
+                        eq(DocumentStatusEnum.APPROVED),
+                        any(OffsetDateTime.class));
+
+        when(approveRegisterRepository
+                .save(any(ApproveRegisterEntity.class)))
+                .thenThrow(new DataAccessException("DB error"){});
+
+        ApproveResponseModelDto result = approveTransactionService
+                .approveOneDocument(
+                        "406b41bcc7ba4fce",
+                        "John Doe",
+                        "Test Document");
+
+        assertNotNull(result);
+        assertEquals(ApproveStatusEnum.ERROR_REGISTRATION_IN_REGISTRY, result.getApproveStatus());
+
+        verify(documentMetadataRepository, times(1))
+                .updateStatusByDocumentIds(
+                        eq("406b41bcc7ba4fce"),
+                        eq(DocumentStatusEnum.APPROVED),
+                        any(OffsetDateTime.class));
+        verify(approveRegisterRepository, times(1))
+                .save(any(ApproveRegisterEntity.class));
+    }
+
+    @Test
+    void approveOneDocument_ConflictWhenNotSubmitted() {
+        when(documentMetadataRepository
+                .findByIdForUpdate("406b41bcc7ba4fce"))
+                .thenReturn(Optional.of(new DocumentMetadataEntity(
+                        "406b41bcc7ba4fce",
+                        "doc_406b41bcc7ba4fce_2026-02-21T09:38:37.459963Z",
+                        "John Doe",
+                        "Test Document",
+                        DocumentStatusEnum.DRAFT,
+                        OffsetDateTime.now(UTC),
+                        OffsetDateTime.now(UTC),
+                        null)));
+
+        ApproveResponseModelDto result = approveTransactionService.approveOneDocument("406b41bcc7ba4fce", "John Doe", "Test Document");
+
+        assertNotNull(result);
+        assertEquals(ApproveStatusEnum.CONFLICT, result.getApproveStatus());
+
+        verify(documentMetadataRepository, never())
+                .updateStatusByDocumentIds(
+                        anyString(),
+                        any(),
+                        any(OffsetDateTime.class));
+        verify(approveRegisterRepository, never())
+                .save(any(ApproveRegisterEntity.class));
+    }
+}
+
