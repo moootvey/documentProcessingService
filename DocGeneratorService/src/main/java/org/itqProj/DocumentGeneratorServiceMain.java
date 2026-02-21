b84d0e99b3788fec91b124bed808b9eee51c8034
moootvey <m.slabukhin@gmail.com>
2026-02-22 01:09:26 +0700

    Add initial configuration and database setup for document processing service

---
+++ DocGeneratorService/src/main/java/org/itqProj/DocumentGeneratorServiceMain.java
@@ -0,0 +1,11 @@
+package org.itqProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentGeneratorServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentGeneratorServiceMain.class, args);
+    }
+}
